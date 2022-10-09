81d43e4fb37f66006663312c91cbc631cf59efe0
Javed Ahmed <dexjaved@gmail.com>
2022-10-09 12:29:19 +0530

    images wip

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import Contact from './components/Contact';
-import Education from './components/Education';
-import Experience from './components/Experience';
-import Navbar from './components/Navbar';
-import Portfolio from './components/Portfolio';
-import Skill from './components/Skill';
-import SocialMedia from './components/SocialMedia';
+import "./App.css";
+import Contact from "./components/Contact";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Skill from "./components/Skill";
+import SocialMedia from "./components/SocialMedia";
 
 function App() {
   return (
-    <div className="App" id="body-header" >
-     <Navbar/>
-     <SocialMedia/>
-     <Skill/>
-     <Experience/>
-     <Education/>
-     <Portfolio/>
-     <Contact/>
+    <div className="App" id="body-header">
+      <Navbar />
+      <SocialMedia />
+      <Skill />
+      <Experience />
+      <Education />
+      <Portfolio />
+      <Contact />
     </div>
   );
 }
